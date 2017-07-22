05afcc0d0aac0e59fa35c36ef8d8f27d1d33c87a
Anisha Garg <Anisha Garg>
2017-07-22 15:20:54 -0600

    commitong

--- src/main/java/restwebapp/restdatabase1/controller/PersonController.java
+++ src/main/java/restwebapp/restdatabase1/controller/PersonController.java
@@ -7,9 +7,11 @@ import restwebapp.restdatabase1.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import restwebapp.restdatabase1.repository.PersonRepository;
 
@@ -26,12 +28,47 @@ public class PersonController {
 		return new ResponseEntity<Person>(person, HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/person")
+	@RequestMapping(value = "/person",method = RequestMethod.GET)
 	public ResponseEntity<List<Person>> getall() {
 		System.out.println("getting person");
 
 		List<Person> persons = (List<Person>) personRepository.findAll();
-		return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
+		
+		ResponseEntity<List<Person>> responsetoSend = new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
+		return responsetoSend;
+	}
+	
+	//   /person/anisha
+	@RequestMapping(value = "/person/{personid}",method = RequestMethod.GET)
+	public ResponseEntity<Person> getOne(@PathVariable("personid") Long pid){
+		 
+		Person person= personRepository.findOne(pid);
+		
+		
+		ResponseEntity<Person> response = new ResponseEntity<Person>(person, HttpStatus.OK);
+		return response;
+		
+	}
+	
+	@SuppressWarnings("unused")
+	@RequestMapping(value = "/person/search",method = RequestMethod.GET)
+	public ResponseEntity<List<Person>> getlastname(@RequestParam(value="lastName", required=false) String lastName, @RequestParam(value="firstName", required=false) String firstName){
+		 
+		List<Person> person = null;
+		System.out.println("names are: " + firstName + " , " + lastName);
+		if(lastName!=null){
+		person= personRepository.findByLastName(lastName);
+		}
+		else if(firstName!=null){
+	    person= personRepository.findByfirstName(firstName);
+		}
+		else if(firstName!=null && lastName!=null){
+	    //person= personRepository.findNames(firstName,lastName);
+		}
+		
+		ResponseEntity<List<Person>> response = new ResponseEntity<List<Person>>(person, HttpStatus.OK);
+		return response;
+		
 	}
 	
 	@RequestMapping("/greeting")
--- src/main/java/restwebapp/restdatabase1/repository/PersonRepository.java
+++ src/main/java/restwebapp/restdatabase1/repository/PersonRepository.java
@@ -2,14 +2,22 @@ package restwebapp.restdatabase1.repository;
 
 import restwebapp.restdatabase1.domain.Person;
 
-import org.springframework.data.domain.Page;
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
-Page<Person> findAll(Pageable pageable);
+//Page<Person> findAll(Pageable pageable);
+	
+	List<Person> findByLastName(String lastName);
+	List<Person> findByfirstName(String firstName);
+	
+	//@Query("from person where firstName:firstName AND lastName:lastName")
+	//List<Person> findNames(String firstName, String lastName);
 
 }
